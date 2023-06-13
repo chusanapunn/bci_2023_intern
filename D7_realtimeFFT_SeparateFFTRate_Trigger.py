9c443a4959cf400b3e4ce0adfc1f41d9e8338237
chusana.r@outlook.com <chusana.r@outlook.com>
2023-06-13 13:08:16 +0700

    trigger 1

--- D7_realtimeFFT_SeparateFFTRate_Trigger.py
+++ D7_realtimeFFT_SeparateFFTRate_Trigger.py
@@ -6,12 +6,12 @@ from pyqtgraph.Qt import QtCore, QtWidgets
 from typing import List
 import scipy.signal as sig
 
-
 # animate plot parameters
 plot_duration = 5  # how many seconds of data to show
 update_interval = 12  # ms between screen updates
 pull_interval = 60 # ms between each pull operation
-# fft_interval = 1000 # ms between each FFT calculation
+fft_interval = 125 # ms between each FFT calculation
+
 
 class Inlet:
     def __init__(self,info:pylsl.StreamInfo):
@@ -23,7 +23,8 @@ class Inlet:
         global fs
         global nyq
         fs = info.nominal_srate()
-        nyq=1/2*fs     
+        nyq=1/2*fs   
+        
 
     def pull_and_plot(self, plot_time: float, plt: pg.PlotItem):
         pass
@@ -36,26 +37,36 @@ class DataInlet(Inlet):
         self.bufsize,self.buffer,empty=set_buffer(info,dtypes)
         global N
         N=int(self.bufsize[0]/fs)
+        
         # signal Curves
         self.curves = [pg.PlotCurveItem(x=empty, y=empty , autoDownsample=True) for _ in range(self.channel_count)]
         self.fftcurves= [pg.PlotCurveItem(x=empty, y=empty , autoDownsample=True) for _ in range(self.channel_count)]
         self.filtcurves= [pg.PlotCurveItem(x=empty, y=empty , autoDownsample=True) for _ in range(self.channel_count)]
-        self.filtfftcurves=[pg.PlotCurveItem(x=empty, y=empty , autoDownsample=True) for _ in range(self.channel_count)]
-        
+        self.filtfftcurves=[pg.PlotCurveItem(x=empty, y=empty , autoDownsample=True)]
+        global triggerVal
+        triggerVal=1.5
+        triggerLine=pg.InfiniteLine(label="SimpleTriggerLine",pos=triggerVal,movable=True,angle=0)
+        triggerLine.setValue(triggerVal)
         for curve in self.curves:
             plt.addItem(curve)
+            # plt.addItem(triggerLine)
         for fftcurve in self.fftcurves:
             fftplt.addItem(fftcurve)
         for filtcurve in self.filtcurves:
             filtplt.addItem(filtcurve)
+            filtplt.addItem(triggerLine)
         for filtfftcurve in self.filtfftcurves:
             filtfftplt.addItem(filtfftcurve)
-        
+            
+
     def pull_and_plot(self, plot_time, ch):
+        global _,ts,this_x,this_y
         _, ts = self.inlet.pull_chunk(timeout=0.0,
                                       max_samples=self.buffer.shape[0],
                                       dest_obj=self.buffer)
         # ts will be empty if no samples were pulled, a list of timestamps otherwise
+        
+        
         if ts:
             ts = np.asarray(ts)
             y = self.buffer[0:ts.size, :]
@@ -69,40 +80,42 @@ class DataInlet(Inlet):
             this_x = np.hstack((old_x[old_offset:], ts[new_offset:]))
             this_y = np.hstack((old_y[old_offset:], y[new_offset:, ch] - ch))
 
-
             self.curves[ch].setData(this_x, this_y)
             # Debug
             # print("X:"+str(this_x),end="")
             # print("Y:"+str(this_y))
-
-            # plot fft
-
-            xlength=len(this_x)
-
-            frequencies=np.fft.fftfreq(xlength,d=1/fs)
-            new_fftX=frequencies[:xlength]
-            new_fftX=abs(new_fftX)
-            new_fftY=np.fft.fft(this_y)
-            new_fftY=np.abs(new_fftY)/(N)
-            # new_fftX=new_fftX.flatten()
-            # new_fftY=new_fftY.flatten()
-
-            self.fftcurves[ch].setData(new_fftX,new_fftY)
-
-            # Filter and plot signal
-            if (ch==1):
-                filtered_thisY=butter_bandstop_filter(this_y,fs,4,17,23)
-            elif (ch==0):
-                filtered_thisY=butter_bandstop_filter(this_y,fs,4,7,13)
-            # filtered_thisY=butter_bandstop_filter(this_y,fs,4,17,23)
-            self.filtcurves[ch].setData(this_x,filtered_thisY)
-
-            # fft of filtered signal
-            filtfft_y=np.fft.fft(filtered_thisY)
-            filtfft_y=np.abs(filtfft_y)/(N)
-            self.filtfftcurves[ch].setData(new_fftX,filtfft_y)
             
 
+            
+    def plot_fft(self,ch):
+        # plot fft
+        global filtered_thisY
+        xlength=len(this_x)
+
+        frequencies=np.fft.fftfreq(xlength,d=1/fs)
+        new_fftX=frequencies[:xlength]
+        new_fftX=abs(new_fftX)
+        new_fftY=np.fft.fft(this_y)
+        new_fftY=np.abs(new_fftY)/(N)
+        # new_fftX=new_fftX.flatten()
+        # new_fftY=new_fftY.flatten()
+
+        self.fftcurves[ch].setData(new_fftX,new_fftY)
+
+        # Filter and plot signal
+        if (ch==1):
+            filtered_thisY=butter_bandstop_filter(this_y,fs,4,17,23)
+        elif (ch==0):
+            filtered_thisY=butter_bandstop_filter(this_y,fs,4,7,13)
+        # filtered_thisY=butter_bandstop_filter(this_y,fs,4,17,23)
+        self.filtcurves[ch].setData(this_x,filtered_thisY)
+
+        # fft of filtered signal
+        filtfft_y=np.fft.fft(filtered_thisY)
+        filtfft_y=np.abs(filtfft_y)/(N)
+        self.filtfftcurves[ch].setData(new_fftX,filtfft_y)
+        if (filtered_thisY[[0]]>triggerVal):
+            print(str(filtered_thisY[0])+" More than "+str(triggerVal))
 
 def set_buffer(info,dtypes):
     bufsize = (2 * math.ceil(info.nominal_srate() * plot_duration), info.channel_count())
@@ -131,6 +144,7 @@ def main():
     win = pg.GraphicsLayoutWidget(show=True, title="lsl multichannel")
     win.resize(1000,600)
     win.setWindowTitle('Multichannel LSL - EEG')
+
 
     for info in streams:
         print(info)
@@ -173,10 +187,13 @@ def main():
             filtch[pltnumber].setXRange(plot_time - plot_duration + fudge_factor, plot_time - fudge_factor)
     def update():
         mintime = pylsl.local_clock() - plot_duration
-        #for pltnumber in range(info.channel_count()):
         for ch,inlet in enumerate(inlets):
             inlet.pull_and_plot(mintime, ch)
-    #def update_fft():
+    def update_fft():
+        for ch,inlet in enumerate(inlets):
+            inlet.plot_fft(ch)
+        print("-----------FFT Update-----------")
+        
     
     # create a timer that will move the view every update_interval ms
     update_timer = QtCore.QTimer()
@@ -188,9 +205,9 @@ def main():
     pull_timer.timeout.connect(update)
     pull_timer.start(pull_interval)
 
-    # fft_timer = QtCore.QTimer()
-    # fft_timer.timeout.connect(update_fft)
-    # fft_timer.start(fft_interval)
+    fft_timer = QtCore.QTimer()
+    fft_timer.timeout.connect(update_fft)
+    fft_timer.start(fft_interval)
 
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):

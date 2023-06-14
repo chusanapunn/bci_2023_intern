df4480bf27e64408e2c618cff4ec2836a3a68189
chusana.r@outlook.com <chusana.r@outlook.com>
2023-06-14 17:39:38 +0700

    error handler update

--- Messaging/tcpExperiment/listener_sender.py
+++ Messaging/tcpExperiment/listener_sender.py
@@ -1,5 +1,5 @@
 import socket               # Import socket module
-
+import time
 s_sender = socket.socket()         # Create a socket object
 host = "127.0.0.1"              # Bind Local
 port = 9030                # Reserve a port for your service.
@@ -12,16 +12,22 @@ s_sender.listen(5)
 
 def sendMessage(cmd,c):
    c.send(cmd.encode())
+   print("--Sent message--")
 
 try:
+    conn, addr =s_sender.accept()
     print ('Open Listening Server', host, port)
     connected=True
-    s_sender.accept()
+     # Establish connection with client.
+    print('Got connection from', addr)
 except s_sender.error :
     connected=False
     print("Connection DOOM, did you open the Client Yet?")
 
-while True:
-    conn, addr = s_sender.accept()     # Establish connection with client.
-    print('Got connection from', addr)
-    sendMessage(command,conn)
+while connected:
+    try:
+        time.sleep(2)
+        sendMessage(command,conn)
+    except ConnectionAbortedError:
+        print("Connection Doom due to something wa")
+        connected = False
